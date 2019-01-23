1b1fe036fdfd8ffa15226c4d9efa729659573d98
Parth M <parth13513@gmail.com>
2019-01-23 18:24:09 +0530

    Initial Commit

---
+++ MondoRestaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MondoRestaurant
+//
+//  Created by Parth Mandaviya on 23/01/19.
+//  Copyright © 2019 Parth Mandaviya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
